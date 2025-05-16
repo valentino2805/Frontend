19f270cfa87dfd033bdeffa9724dadf820579a6a
Mevi1217 <melgarejogomezmarcia@gmail.com>
2025-05-16 08:31:38 -0500

    feat: i18n sustainableactions

--- src/app/susteinableActions/components/actions-create-and-edit/actions-create-and-edit.component.ts
+++ src/app/susteinableActions/components/actions-create-and-edit/actions-create-and-edit.component.ts
@@ -2,12 +2,13 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {Action} from "../../model/action.entity";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-actions-create-and-edit',
   standalone: true,
   imports: [
-    CommonModule, FormsModule
+    CommonModule, FormsModule, TranslateModule
   ],
   templateUrl: './actions-create-and-edit.component.html',
   styleUrl: './actions-create-and-edit.component.css'
--- src/app/susteinableActions/components/tips-list/tips-list.component.ts
+++ src/app/susteinableActions/components/tips-list/tips-list.component.ts
@@ -3,11 +3,12 @@ import {TipsCardComponent} from "../tips-card/tips-card.component";
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import { Action } from '../../model/action.entity';
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-tips-list',
   standalone: true,
-  imports: [CommonModule, TipsCardComponent, FormsModule],
+  imports: [CommonModule, TipsCardComponent, FormsModule, TranslateModule],
   templateUrl: './tips-list.component.html',
   styleUrl: './tips-list.component.css'
 })
@@ -21,12 +22,27 @@ export class TipsListComponent implements OnInit, OnChanges {
   ngOnInit(): void {
 
   }
-
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['externalActions']) {
-      this.actions = [...[new Action({ id: 1, title: 'Reducir Plástico', description: 'Evita plásticos de un solo uso', type: 'almacenamiento' }),
-          new Action({ id: 2, title: 'Apaga las luces', description: 'Ahorra energía eléctrica', type: 'mejora operativa' }),
-          new Action({ id: 3, title: 'Camina más', description: 'Reduce tu huella de carbono', type: 'normativas' }),],
+      this.actions = [
+        new Action({
+          id: 1,
+          title: 'Implementa contenedores diferenciados',
+          description: 'Separa residuos en reciclables, orgánicos y peligrosos dentro de la empresa.',
+          type: 'almacenamiento'
+        }),
+        new Action({
+          id: 2,
+          title: 'Optimiza el uso del aire acondicionado',
+          description: 'Programa horarios y temperatura eficiente para reducir el consumo energético.',
+          type: 'mejora operativa'
+        }),
+        new Action({
+          id: 3,
+          title: 'Promueve la movilidad sostenible',
+          description: 'Incentiva el uso de bicicletas o transporte público entre colaboradores.',
+          type: 'normativas'
+        }),
         ...this.externalActions
       ];
       this.applyFilters();
