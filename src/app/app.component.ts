d1fedec66bb77175556a9375e90a55f6a3f3aaff
SoyValzzz <u20211a962@upc.edu.pe>
2025-05-18 00:14:51 -0500

    feat: implement authentication buttons and role-based menu options in the main application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,16 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { CommonModule } from '@angular/common';
+import { AuthService } from './users/services/auth.service';
+import { filter } from 'rxjs/operators';
+import { NavigationEnd } from '@angular/router';
+
+interface MenuOption {
+  icon: string;
+  path: string;
+  title: string;
+  roles?: string[];
+}
 
 @Component({
   selector: 'app-root',
@@ -29,41 +39,77 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
   title = 'CleanView';
   @ViewChild(MatSidenav, { static: true }) sidenav!: MatSidenav;
+  showAuthButtons = true;
+  showUserIcon = true;
   activeOption: string = '';
   isSidenavOpen = true;
 
-  otherOptions = [
+  allOptions: MenuOption[] = [
     { icon: 'https://cdn-icons-png.flaticon.com/512/25/25694.png', path: '/home', title: 'homeTitle' },
-    { icon: 'https://cdn-icons-png.flaticon.com/512/3917/3917028.png', path: '/controlPanel', title: 'controlPanelTitle' },
-    { icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910768.png', path: '/sustainableActions', title: 'sustainableActionsTitle' },
-    { icon: 'https://cdn-icons-png.flaticon.com/512/535/535239.png', path: '/collectionPoints', title: 'collectionPointsTitle' },
-    { icon: 'https://cdn-icons-png.flaticon.com/512/992/992700.png', path: '/reports', title: 'reportsTitle' },
+    { icon: 'https://cdn-icons-png.flaticon.com/512/3917/3917028.png', path: '/controlPanel', title: 'controlPanelTitle', roles: ['empresa'] },
+    {icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828970.png', path: '/rewards', title: 'rewards', roles: ['admin'] },
+    { icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910768.png', path: '/sustainableActions', title: 'sustainableActionsTitle', roles: ['empresa', 'admin'] },
+    { icon: 'https://cdn-icons-png.flaticon.com/512/535/535239.png', path: '/collectionPoints', title: 'collectionPointsTitle', roles: ['empresa', 'admin'] },
+    { icon: 'https://cdn-icons-png.flaticon.com/512/992/992700.png', path: '/reports', title: 'reportsTitle', roles: ['empresa', 'admin'] }
   ];
+
+  otherOptions: MenuOption[] = [];
 
   constructor(
     private translate: TranslateService,
     private observer: BreakpointObserver,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {
     // Configuración de traducción
     translate.setDefaultLang('en');
     translate.use('en');
+
+
   }
 
   ngOnInit(): void {
-    // Configuración de la vista de Sidenav dependiendo del tamaño de la pantalla
     this.observer.observe(['(max-width: 1280px)']).subscribe((response) => {
       if (response.matches) {
         this.sidenav.mode = 'over';
         this.sidenav.close();
       } else {
         this.sidenav.mode = 'side';
-        this.sidenav.open();
+        //this.sidenav.open();
       }
     });
+
+    this.authService.user$.subscribe(() => {
+      this.updateMenu();
+    });
+
+    this.updateMenu();
+
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      const url = event.urlAfterRedirects;
+
+      if (url === '/login' || url === '/register') {
+        this.showAuthButtons = true;
+        this.showUserIcon = false;
+      } else {
+        this.showAuthButtons = false;
+        this.showUserIcon = true;
+      }
+    });
+  }
+
+  updateMenu() {
+    const role = this.authService.getRole();
+    this.otherOptions = this.allOptions.filter(option =>
+      !option.roles || option.roles.includes(role!)
+    );
   }
 
   toggleSidenav() {
@@ -74,4 +120,13 @@ export class AppComponent implements OnInit {
   setActiveOption(option: string) {
     this.activeOption = option;
   }
+  onSignIn() {
+    this.router.navigate(['/login']);
+  }
+
+  onSignUp() {
+    this.router.navigate(['/register']);
+  }
+
 }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,17 +5,28 @@ import { HomeComponent } from './public/pages/home/home.component';
 const ControlPanelComponent = () => import('./controlPanel/pages/controlPanel.component').then(x => x.ControlPanelComponent);
 const CollectionPointsComponent=()=> import ('./collection/pages/collection-points/collection-points.component').then(x => x.CollectionPointsPage);
 const SustenaibleActionsComponent=()=> import ('./susteinableActions/pages/sustainable-actions/sustainable-actions.component').then(x => x.SustainableActionsComponent);
+import { LoginComponent } from './users/pages/login/login.component';
+import { AuthGuard } from './users/services/auth.guard';
+import { RegisterComponent } from './users/pages/register/register.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
 
-  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AuthGuard]
+  },
+
 
   { path: 'controlPanel', loadComponent: ControlPanelComponent, title: 'Control Panel' },
   { path: 'collectionPoints', loadComponent: CollectionPointsComponent, title: 'Control Panel' },
   { path: 'reports', component: ReportsComponent },
   { path: 'sustainableActions', loadComponent: SustenaibleActionsComponent, title: 'Sustenaible Actions' },
 
-  { path: '', component: PageNotFoundComponent },
+
 
 
 ];
