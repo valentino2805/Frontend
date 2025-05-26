b111a40965cbf158ed3ed8d6db10ccd9595ae432
Gabriel Gordon <116988979+Silent343@users.noreply.github.com>
2025-05-26 17:56:06 -0500

    feat(login): add import NgIf

--- src/app/users/pages/login/login.component.ts
+++ src/app/users/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import {TranslateModule} from "@ngx-translate/core";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-login',
