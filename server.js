8882cff6c887137c307d369e3af9f27a244897ee
SoyValzzz <valentino2805@gmail.com>
2025-07-08 08:32:50 -0500

    feat: update server configuration to use Express and serve static files

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// Puerto proporcionado por Railway u 8080 como fallback
+// __dirname workaround
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
 const port = process.env.PORT || 8080;
 
-// Reemplaza esto con el nombre de tu proyecto compilado
 const distFolder = path.join(__dirname, 'dist/stadistics-bounded-context');
-
-// Servir archivos estáticos desde la carpeta de producción
 app.use(express.static(distFolder));
 
-// Redirigir todas las rutas al index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(distFolder, 'index.html'));
 });
 
-// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
 });
